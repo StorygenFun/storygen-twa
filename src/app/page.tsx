ecdd4fee1b626afce33b4b6ce9b21c2c3a7e7dfd
Sergey Eroshenkov <itechmeat@gmail.com>
2024-04-25 21:51:38 +0200

    Reorganaze

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { OpenAiConnector } from '@/components/OpenAiConnector'
+
+export default function Home() {
+  return (
+    <main>
+      <div>HOMEPAGE!!</div>
+      <OpenAiConnector />
+    </main>
+  )
+}
