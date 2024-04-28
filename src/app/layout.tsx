f8a0850e9aa57656351a62baf68ee028ef616ee6
Sergey Eroshenkov <itechmeat@gmail.com>
2024-04-28 11:38:44 +0200

    Console siteUrl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   const locale = getLocale()
+  console.log('siteUrl:', process.env.NEXT_PUBLIC_BASE_URL)
 
   return (
     <html lang={locale}>
