53d09a943a648ad5727c67945a603bd59ede425a
Sergey Eroshenkov <itechmeat@gmail.com>
2024-11-06 22:39:55 +0100

    гз

--- src/features/story/Story/StoryController.tsx
+++ src/features/story/Story/StoryController.tsx
@@ -213,8 +213,8 @@ export const Story: FC<StoryProps> = ({ storyId, siteUrl }) => {
 
   const handleStartGeneration = async (currentStory: IStory) => {
     const cost = Number(fromNano(calculateStoryGenerationCost(currentStory.scenesNum || 1)))
-    const canUsePromoCode = promoCodeBalance && promoCodeBalance >= cost
-    // const canUsePromoCode = promoCodeBalance && promoCodeBalance >= cost && false
+    // const canUsePromoCode = promoCodeBalance && promoCodeBalance >= cost
+    const canUsePromoCode = promoCodeBalance && promoCodeBalance >= cost && false
 
     if (currentStory.payment_transaction || canUsePromoCode) {
       changeCurrentStep(GenerationStep.Brief)
